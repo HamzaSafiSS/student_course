2233b2defd156b55a927b44db5a0c72a6fa61931
Hamza Safi <safihamza395@gmail.com>
2026-03-06 00:44:58 +0300

    Project initialized with clean architecture and MySQL connection

---
+++ src/test/java/com/hamza/studentcourse/StudentCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamza.studentcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
